867b06354a8be526c0f01bea07c24aa52272e695
Musicus69 <53125766+Musicus69@users.noreply.github.com>
2020-07-13 17:50:40 -0400

    Add files via upload

---
+++ scripts/game.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('button')[1].onclick = function() {
+    question = document.querySelector('.askQuestion');
+    question.textContent = "When was Beethoven born?"
+    guess = document.createElement('input');
+
+}
